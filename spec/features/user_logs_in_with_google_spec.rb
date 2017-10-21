323df2bfc8fa6dfa86f881b141fcff61243eba9b
Matty Burns <matty@Mattys-MacBook-Air.local>
2017-10-21 13:06:48 -0400

    Added omniauth initializer

--- spec/features/user_logs_in_with_google_spec.rb
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -4,8 +4,19 @@ require 'rails_helper'
 def stub_omniauth
   OmniAuth.config.test_mode = true
   OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
-
-    })
+    provider: "google",
+    uid: "12345678910",
+    info: {
+      email: "jesse@mountainmantechnologies.com",
+      first_name: "Jesse",
+      last_name: "Spevack"
+    },
+    credentials: {
+      token: "abcdefg12345",
+      refresh_token: "12345abcdefg",
+      expires_at: DateTime.now,
+    }
+  })
 end
 
 
